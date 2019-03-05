6d2151236369b843b067c1597db75b7dd706e9b8
Giovani de Sousa Leite <sousa_giovani@hotmail.com>
2019-03-05 09:20:05 -0300

    Install and setup croods on example

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { Provider } from 'react-redux'
+import { New, Provider as CroodsProvider } from 'croods'
+import { Grid } from '@material-ui/core'
 
-import { SignIn } from 'seasoned-auth-forms-web'
+import { SignIn, SignUp } from 'seasoned-auth-forms-web'
+
+import store from './store/store'
 
 const theme = createMuiTheme({})
 
-export default class App extends Component {
-  render() {
-    return (
-      <MuiThemeProvider theme={theme}>
-        <SignIn onSubmit={values => console.log(values)} />
-      </MuiThemeProvider>
-    )
-  }
+export default props => {
+  return (
+    <Provider store={store}>
+      <CroodsProvider
+        baseUrl={
+          'https://cors-redirect.herokuapp.com/https://postb.in/lAZ7NVjh'
+        }
+      >
+        <MuiThemeProvider theme={theme}>
+          <Grid container justify="space-around">
+            <New
+              name="signin"
+              path="/"
+              render={({ create, creating, error }) => (
+                <SignIn
+                  onSubmit={create}
+                  submitting={creating}
+                  submitError={error}
+                />
+              )}
+            />
+            <New
+              name="signup"
+              path="/"
+              render={({ create, creating, error }) => (
+                <SignUp
+                  onSubmit={create}
+                  submitting={creating}
+                  submitError={error}
+                />
+              )}
+            />
+          </Grid>
+        </MuiThemeProvider>
+      </CroodsProvider>
+    </Provider>
+  )
 }
