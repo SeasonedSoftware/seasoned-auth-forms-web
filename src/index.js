48da5b55c1379ca1a727bbdc8d58d959d4bbea20
Giovani de Sousa Leite <sousa_giovani@hotmail.com>
2019-03-07 11:34:04 -0300

    fix import on index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import SeasonedSignIn from './signin/Container'
 import SeasonedSignUp from './signup/Container'
-import SeasonedForgotPassword from './ForgotPassword/Container'
+import SeasonedForgotPassword from './forgotPassword/Container'
 import SeasonedNewPassword from './newPassword/Container'
 
 export const SignIn = SeasonedSignIn
